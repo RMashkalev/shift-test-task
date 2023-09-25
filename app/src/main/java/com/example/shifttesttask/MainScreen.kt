45e993ebfa311e5e385fb7ebffadab5325cf5473
RMashkalev <rmashkalev@mail.ru>
2023-09-25 20:01:08 +0700

    add save data and rework password and name checks

--- app/src/main/java/com/example/shifttesttask/MainScreen.kt
+++ app/src/main/java/com/example/shifttesttask/MainScreen.kt
@@ -1,6 +1,7 @@
 package com.example.shifttesttask
 
 import android.app.AlertDialog
+import android.content.Context
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
@@ -26,6 +27,17 @@ class MainScreen : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
+
+        greetingButton()
+        exitButton()
+        saveData(personData.name.value.toString(), personData.surname.value.toString(),
+            personData.day.value.toString(), personData.month.value.toString(),
+            personData.year.value.toString(), personData.password.value.toString(),
+            personData.repeatPassword.value.toString(),
+            personData.allChecksPassed.value == true)
+    }
+
+    private fun greetingButton() {
         mainScreenBinding.greetingButton.setOnClickListener {
             val dialog = AlertDialog.Builder(activity)
             val title = R.string.greetingButton
@@ -36,12 +48,35 @@ class MainScreen : Fragment() {
                     + personData.surname.value)
             dialog.show()
         }
+    }
 
+    private fun exitButton() {
         mainScreenBinding.exitButton.setOnClickListener {
             activity?.supportFragmentManager?.beginTransaction()
                 ?.replace(R.id.mainFrame, Registration.newInstance())
                 ?.commit()
+            saveData()
         }
+    }
+
+    private fun saveData(name: String = "", surname: String = "", day: String = "",
+                         month: String = "", year: String = "", password: String = "",
+                         repeatPassword: String = "", allChecks: Boolean = false) {
+        super.onDestroy()
+        val sharedPreferences = activity
+            ?.getSharedPreferences(R.string.shared_preferences.toString(), Context.MODE_PRIVATE)
+        val editor = sharedPreferences?.edit()
+        editor?.putString(R.string.user_name.toString(), name)
+        editor?.putString(R.string.user_surname.toString(), surname)
+        editor?.putString(R.string.user_day.toString(), day)
+        editor?.putString(R.string.user_month.toString(), month)
+        editor?.putString(R.string.user_year.toString(), year)
+        editor?.putString(R.string.user_password.toString(), password)
+        editor?.putString(R.string.user_repeat_password.toString(), repeatPassword)
+        editor?.putBoolean(R.string.user_all_checks.toString(),
+            allChecks
+        )
+        editor?.apply()
     }
 
     companion object {
--- app/src/main/java/com/example/shifttesttask/Registration.kt
+++ app/src/main/java/com/example/shifttesttask/Registration.kt
@@ -1,8 +1,10 @@
 package com.example.shifttesttask
 
+import android.content.Context
 import android.os.Bundle
 import android.text.Editable
 import android.text.TextWatcher
+import android.util.Log
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
@@ -34,30 +36,51 @@ class Registration : Fragment() {
 
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
-        initPersonData()
+        loadingData()
         inputFields()
         birthdaySelect()
         registrationButton()
     }
 
-    private fun initPersonData() {
-        personData.name.value = ""
-        personData.surname.value = ""
-        personData.day.value = ""
-        personData.month.value = ""
-        personData.year.value = ""
-        personData.password.value = ""
-        personData.repeatPassword.value = ""
+    private fun loadingData() {
+        val sharedPreferences = activity
+            ?.getSharedPreferences(R.string.shared_preferences.toString(), Context.MODE_PRIVATE)
+        personData.name.value =
+            sharedPreferences?.getString(R.string.user_name.toString(), "")
+        personData.surname.value =
+            sharedPreferences?.getString(R.string.user_surname.toString(), "")
+        personData.day.value =
+            sharedPreferences?.getString(R.string.user_day.toString(), "")
+        personData.month.value =
+            sharedPreferences?.getString(R.string.user_month.toString(), "")
+        personData.year.value =
+            sharedPreferences?.getString(R.string.user_year.toString(), "")
+        personData.password.value =
+            sharedPreferences?.getString(R.string.user_password.toString(), "")
+        personData.repeatPassword.value =
+            sharedPreferences?.getString(R.string.user_repeat_password.toString(), "")
+        personData.allChecksPassed.value =
+            sharedPreferences?.getBoolean(R.string.user_all_checks.toString(), false)
+        personData.birthdayCheck.value =
+            sharedPreferences?.getBoolean(R.string.user_birthday_check.toString(), false)
+        Log.d("debug", personData.allChecksPassed.value.toString())
+        if (personData.allChecksPassed.value == true) {
+            activity?.supportFragmentManager?.beginTransaction()
+                ?.replace(R.id.mainFrame, MainScreen.newInstance())
+                ?.commit()
+        }
     }
 
     private fun inputFields() {
         registrationBinding.inputName.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(result: CharSequence?, start: Int, before: Int, count: Int) {
             }
+
             override fun afterTextChanged(result: Editable?) {
-                if (result.toString().length in 2..75) {
+                if (nameCheck(result.toString())) {
                     personData.name.value = result.toString()
                     registrationBinding.inputName.setBackgroundResource(positive_edit_text_theme)
                     registrationBinding.nameErrorMessage.text = ""
@@ -74,10 +97,12 @@ class Registration : Fragment() {
         registrationBinding.inputSurname.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(result: CharSequence?, start: Int, before: Int, count: Int) {
             }
+
             override fun afterTextChanged(result: Editable?) {
-                if (result.toString().length in 3..75) {
+                if (nameCheck(result.toString())) {
                     personData.surname.value = result.toString()
                     registrationBinding.inputSurname.setBackgroundResource(positive_edit_text_theme)
                     registrationBinding.surnameErrorMessage.text = ""
@@ -85,7 +110,7 @@ class Registration : Fragment() {
                     personData.surname.value = ""
                     registrationBinding.inputSurname.setBackgroundResource(negative_edit_text_theme)
                     registrationBinding.surnameErrorMessage.text =
-                        resources.getString(R.string.negative_surname)
+                        resources.getString(R.string.negative_name)
                 }
                 fieldsCheck()
             }
@@ -94,8 +119,10 @@ class Registration : Fragment() {
         registrationBinding.inputFirstPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(result: CharSequence?, start: Int, before: Int, count: Int) {
             }
+
             override fun afterTextChanged(result: Editable?) {
                 if (passwordCheck(result.toString())) {
                     personData.password.value = result.toString()
@@ -116,8 +143,10 @@ class Registration : Fragment() {
         registrationBinding.inputSecondPassword.addTextChangedListener(object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
+
             override fun onTextChanged(result: CharSequence?, start: Int, before: Int, count: Int) {
             }
+
             override fun afterTextChanged(result: Editable?) {
                 if (result.toString() == personData.password.value.toString()) {
                     personData.repeatPassword.value = result.toString()
@@ -147,12 +176,15 @@ class Registration : Fragment() {
                 && personData.repeatPassword.value != "")
     }
 
+    fun nameCheck(name: String): Boolean {
+        if (name.length < 2 || name.length > 75) return false
+        if (name.contains(Regex("[^A-Za-z]"))) return false
+        return true
+    }
+
     fun passwordCheck(password: String): Boolean {
-        val symbols = "!@#$%^&*()"
-        if (password.firstOrNull { it.isDigit() } == null) return false
-        if (password.firstOrNull { it.isLetter() } == null) return false
-        if (password.firstOrNull { it.isWhitespace()} != null) return false
-        if (password.firstOrNull { symbols.contains(it)} == null) return false
+        if (password.length < 8 || password.length > 100) return false
+        if (password.contains(Regex("^(?=.*[0-9])(?=.*[a-z])(?=.*[A-Z])(?=.*[@#\$%^&+=])(?=\\\\S+\$).{4,}\$"))) return false
         return true
     }
 
@@ -164,76 +196,103 @@ class Registration : Fragment() {
         for (i in 1..12) months.add(i.toString())
         for (i in 1930..2023) years.add(i.toString())
         val daysAdapter = activity?.let {
-            ArrayAdapter<String>(it,
+            ArrayAdapter<String>(
+                it,
                 androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item,
-                days)
+                days
+            )
         }
         val monthsAdapter = activity?.let {
-            ArrayAdapter<String>(it,
+            ArrayAdapter<String>(
+                it,
                 androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item,
-                months)
+                months
+            )
         }
         val yearsAdapter = activity?.let {
-            ArrayAdapter<String>(it,
+            ArrayAdapter<String>(
+                it,
                 androidx.constraintlayout.widget.R.layout.support_simple_spinner_dropdown_item,
-                years)
+                years
+            )
         }
         registrationBinding.day.adapter = daysAdapter
         registrationBinding.month.adapter = monthsAdapter
         registrationBinding.year.adapter = yearsAdapter
 
-        registrationBinding.day.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                registrationBinding.day.setBackgroundResource(positive_edit_text_theme)
-                personData.day.value = days[pos]
-                birthdayCheck()
-                fieldsCheck()
-            }
+        registrationBinding.day.onItemSelectedListener =
+            (object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    pos: Int,
+                    id: Long
+                ) {
+                    registrationBinding.day.setBackgroundResource(positive_edit_text_theme)
+                    personData.day.value = days[pos]
+                    birthdayCheck()
+                    fieldsCheck()
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                registrationBinding.day.setBackgroundResource(negative_edit_text_theme)
-            }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    registrationBinding.day.setBackgroundResource(negative_edit_text_theme)
+                }
 
-        })
-        registrationBinding.month.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                registrationBinding.month.setBackgroundResource(positive_edit_text_theme)
-                personData.month.value = months[pos]
-                birthdayCheck()
-                fieldsCheck()
-            }
+            })
+        registrationBinding.month.onItemSelectedListener =
+            (object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    pos: Int,
+                    id: Long
+                ) {
+                    registrationBinding.month.setBackgroundResource(positive_edit_text_theme)
+                    personData.month.value = months[pos]
+                    birthdayCheck()
+                    fieldsCheck()
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                registrationBinding.month.setBackgroundResource(negative_edit_text_theme)
-            }
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    registrationBinding.month.setBackgroundResource(negative_edit_text_theme)
+                }
 
-        })
-        registrationBinding.year.onItemSelectedListener = (object : AdapterView.OnItemSelectedListener {
-            override fun onItemSelected(parent: AdapterView<*>?, view: View?, pos: Int, id: Long) {
-                registrationBinding.year.setBackgroundResource(positive_edit_text_theme)
-                personData.year.value = years[pos]
-                birthdayCheck()
-                fieldsCheck()
-            }
+            })
+        registrationBinding.year.onItemSelectedListener =
+            (object : AdapterView.OnItemSelectedListener {
+                override fun onItemSelected(
+                    parent: AdapterView<*>?,
+                    view: View?,
+                    pos: Int,
+                    id: Long
+                ) {
+                    registrationBinding.year.setBackgroundResource(positive_edit_text_theme)
+                    personData.year.value = years[pos]
+                    birthdayCheck()
+                    fieldsCheck()
+                }
 
-            override fun onNothingSelected(parent: AdapterView<*>?) {
-                registrationBinding.year.setBackgroundResource(negative_edit_text_theme)
-            }
-        })
+                override fun onNothingSelected(parent: AdapterView<*>?) {
+                    registrationBinding.year.setBackgroundResource(negative_edit_text_theme)
+                }
+            })
     }
 
     private fun birthdayCheck() {
         if (personData.day.value == ""
             || personData.month.value == ""
-            || personData.year.value == "") {
+            || personData.year.value == ""
+        ) {
             return
         }
         try {
             val formatter = SimpleDateFormat("dd-MM-yyyy", Locale.getDefault())
             formatter.isLenient = false
-            formatter.parse(personData.day.value.toString() + "-"
-                    + personData.month.value.toString() + "-"
-                    + personData.year.value.toString())
+            formatter.parse(
+                personData.day.value.toString() + "-"
+                        + personData.month.value.toString() + "-"
+                        + personData.year.value.toString()
+            )
             registrationBinding.day.setBackgroundResource(positive_edit_text_theme)
             registrationBinding.month.setBackgroundResource(positive_edit_text_theme)
             registrationBinding.year.setBackgroundResource(positive_edit_text_theme)
